cd388bf92a9bdf9b12f4daa3ae32b2886a31984f
marius <marius.contoli-legay@epitech.eu>
2020-03-28 16:01:24 +0100

    fix

--- src/get.c
+++ src/get.c
@@ -71,7 +71,9 @@ int binding_key(tetris_t *tetris, int ac, char **av)
 {
     int index = 1;
     while (index < ac) {
-        if (av[index][0] == '-' && av[index][1] != 'D') {
+        if (av[index][0 == '-'] && av[index][1] == 'D')
+            index += 1;
+        if (av[index][0] == '-') {
             bind_key(tetris, av, index);
             index += 2;
         }
